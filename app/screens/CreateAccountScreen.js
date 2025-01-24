5471aa9cc6000650d34d4da4ba1f9e6b7d12a3c2
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2025-01-24 17:05:12 -0300

    convertidor moneda

--- app/screens/CreateAccountScreen.js
+++ app/screens/CreateAccountScreen.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Alert, Platform } from 'react-native';
 import { useRouter, useLocalSearchParams } from "expo-router";
 import { GuardarUsuario } from "../../components/GuardarUsuario";
+import DropDownPicker from 'react-native-dropdown-picker';
+
+const countryCurrencyOptions = [
+  { label: 'Chile (CLP)', value: 'CLP' },
+  { label: 'United States (USD)', value: 'USD' },
+  { label: 'Argentina (ARS)', value: 'ARS' },
+  { label: 'Brazil (BRL)', value: 'BRL' },
+  { label: 'Canada (CAD)', value: 'CAD' },
+  { label: 'China (CNY)', value: 'CNY' },
+  { label: 'Eurozone (EUR)', value: 'EUR' },
+  { label: 'India (INR)', value: 'INR' },
+  { label: 'Japan (JPY)', value: 'JPY' },
+  { label: 'Mexico (MXN)', value: 'MXN' },
+  { label: 'Russia (RUB)', value: 'RUB' },
+  { label: 'South Africa (ZAR)', value: 'ZAR' },
+  { label: 'Australia (AUD)', value: 'AUD' },
+  { label: 'New Zealand (NZD)', value: 'NZD' },
+  { label: 'United Kingdom (GBP)', value: 'GBP' },
+  { label: 'South Korea (KRW)', value: 'KRW' },
+  { label: 'Switzerland (CHF)', value: 'CHF' },
+  { label: 'Turkey (TRY)', value: 'TRY' },
+  { label: 'Norway (NOK)', value: 'NOK' },
+  { label: 'Sweden (SEK)', value: 'SEK' },
+  { label: 'Denmark (DKK)', value: 'DKK' },
+  { label: 'Poland (PLN)', value: 'PLN' },
+  { label: 'Saudi Arabia (SAR)', value: 'SAR' },
+  { label: 'Indonesia (IDR)', value: 'IDR' },
+  { label: 'Thailand (THB)', value: 'THB' },
+  { label: 'Malaysia (MYR)', value: 'MYR' },
+  { label: 'Singapore (SGD)', value: 'SGD' },
+  { label: 'Hong Kong (HKD)', value: 'HKD' },
+  { label: 'Philippines (PHP)', value: 'PHP' },
+  { label: 'Israel (ILS)', value: 'ILS' },
+  // Agrega más países y monedas según sea necesario
+];
 
 export default function CreateAccountScreen() {
   const router = useRouter();
@@ -13,9 +48,11 @@ export default function CreateAccountScreen() {
   const [cigarettesPerDay, setCigarettesPerDay] = useState('');
   const [cigarettesPerPack, setCigarettesPerPack] = useState('');
   const [packPrice, setPackPrice] = useState('');
+  const [currency, setCurrency] = useState(''); // Estado para la moneda
   const [questionStep, setQuestionStep] = useState(1);
   const [yearlySavings, setYearlySavings] = useState(null);
   const [cigarettesPerYear, setCigarettesPerYear] = useState(null);
+  const [open, setOpen] = useState(false); // Estado para el dropdown
 
   const toggleReason = (option) => {
     if (reasons.includes(option)) {
@@ -27,12 +64,18 @@ export default function CreateAccountScreen() {
 
   const handleContinue = () => {
     if (questionStep === 1) {
-      if (reasons.length === 0) {
-        Alert.alert('Por favor, elige al menos una razón para dejar de fumar.');
+      if (!currency) {
+        Alert.alert('Por favor, seleccione un tipo de moneda.');
         return;
       }
       setQuestionStep(2); // Pasar a la siguiente pregunta
     } else if (questionStep === 2) {
+      if (reasons.length === 0) {
+        Alert.alert('Por favor, elige al menos una razón para dejar de fumar.');
+        return;
+      }
+      setQuestionStep(3); // Pasar a la siguiente pregunta
+    } else if (questionStep === 3) {
       if (!age || !yearsSmoking) {
         Alert.alert('Por favor, ingrese tanto su edad como los años fumando.');
         return;
@@ -45,20 +88,20 @@ export default function CreateAccountScreen() {
         Alert.alert('Los años fumando no pueden ser mayores que su edad.');
         return;
       }
-      setQuestionStep(3); // Pasar a la siguiente pregunta
-    } else if (questionStep === 3) {
+      setQuestionStep(4); // Pasar a la siguiente pregunta
+    } else if (questionStep === 4) {
       if (!cigarettesPerDay || isNaN(cigarettesPerDay)) {
         Alert.alert('Por favor, ingrese un número válido de cigarrillos al día.');
         return;
       }
-      setQuestionStep(4); // Pasar a la siguiente pregunta
-    } else if (questionStep === 4) {
+      setQuestionStep(5); // Pasar a la siguiente pregunta
+    } else if (questionStep === 5) {
       if (!cigarettesPerPack || isNaN(cigarettesPerPack)) {
         Alert.alert('Por favor, ingrese un número válido de cigarrillos por cajetilla.');
         return;
       }
-      setQuestionStep(5); // Pasar a la siguiente pregunta
-    } else if (questionStep === 5) {
+      setQuestionStep(6); // Pasar a la siguiente pregunta
+    } else if (questionStep === 6) {
       if (!packPrice || isNaN(packPrice)) {
         Alert.alert('Por favor, ingrese un precio válido de la cajetilla.');
         return;
@@ -73,7 +116,7 @@ export default function CreateAccountScreen() {
       setYearlySavings(yearlySavingsAmount.toFixed(2)); // Guardamos el ahorro anual calculado
       setCigarettesPerYear(totalCigarettesSaved); // Guardamos la cantidad de cigarrillos ahorrados
 
-      setQuestionStep(6); // Mostrar resumen del ahorro
+      setQuestionStep(7); // Mostrar resumen del ahorro
     }
   };
 
@@ -98,11 +141,33 @@ export default function CreateAccountScreen() {
     }
   };
 
+  const formatCurrency = (amount) => {
+    const options = { style: 'currency', currency };
+    return new Intl.NumberFormat('es-CL', options).format(amount);
+  };
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
         <View style={styles.overlay}>
           {questionStep === 1 && (
+            <View style={styles.questionContainer}>
+              <Text style={styles.title}>Seleccione su país y tipo de moneda</Text>
+              <DropDownPicker
+                open={open}
+                value={currency}
+                items={countryCurrencyOptions}
+                setOpen={setOpen}
+                setValue={setCurrency}
+                setItems={() => {}}
+                placeholder="Seleccione un país y moneda..."
+                style={styles.dropdown}
+                dropDownContainerStyle={styles.dropdownContainer}
+              />
+            </View>
+          )}
+
+          {questionStep === 2 && (
             <View style={styles.questionContainer}>
               <Text style={styles.title}>¿Por qué quieres dejar de fumar?</Text>
               <View style={styles.optionsContainer}>
@@ -119,7 +184,7 @@ export default function CreateAccountScreen() {
             </View>
           )}
 
-          {questionStep === 2 && (
+          {questionStep === 3 && (
             <View style={styles.questionContainer}>
               <Text style={styles.title}>¿Cuántos años tienes?</Text>
               <TextInput
@@ -140,7 +205,7 @@ export default function CreateAccountScreen() {
             </View>
           )}
 
-          {questionStep === 3 && (
+          {questionStep === 4 && (
             <View style={styles.questionContainer}>
               <Text style={styles.title}>¿Cuántos cigarrillos fumas al día?</Text>
               <TextInput
@@ -153,7 +218,7 @@ export default function CreateAccountScreen() {
             </View>
           )}
 
-          {questionStep === 4 && (
+          {questionStep === 5 && (
             <View style={styles.questionContainer}>
               <Text style={styles.title}>¿Cuántos cigarrillos tenía la cajetilla?</Text>
               <TextInput
@@ -166,7 +231,7 @@ export default function CreateAccountScreen() {
             </View>
           )}
 
-          {questionStep === 5 && (
+          {questionStep === 6 && (
             <View style={styles.questionContainer}>
               <Text style={styles.title}>¿Cuánto costaba la cajetilla?</Text>
               <TextInput
@@ -179,13 +244,13 @@ export default function CreateAccountScreen() {
             </View>
           )}
 
-          {questionStep === 6 && (
+          {questionStep === 7 && (
             <View style={styles.resultContainer}>
               <Text style={styles.title}>¡Felicidades por dar el primer paso!</Text>
               <Text style={styles.resultText}>
                 Si dejas de fumar, podrías ahorrar aproximadamente:
               </Text>
-              <Text style={styles.resultAmount}>${yearlySavings} al año</Text>
+              <Text style={styles.resultAmount}>{formatCurrency(yearlySavings)} al año</Text>
               <Text style={styles.resultText}>¡Eso es un gran ahorro!</Text>
               <Text style={styles.resultText}>
                 Además, dejarías de fumar aproximadamente {cigarettesPerYear} cigarrillos al año.
@@ -193,8 +258,8 @@ export default function CreateAccountScreen() {
             </View>
           )}
 
-          <TouchableOpacity style={styles.button} onPress={questionStep === 6 ? handleFinish : handleContinue}>
-            <Text style={styles.buttonText}>{questionStep === 6 ? 'Ir a Perfil' : 'Continuar'}</Text>
+          <TouchableOpacity style={styles.button} onPress={questionStep === 7 ? handleFinish : handleContinue}>
+            <Text style={styles.buttonText}>{questionStep === 7 ? 'Ir a Perfil' : 'Continuar'}</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -222,6 +287,8 @@ const styles = StyleSheet.create({
   questionContainer: {
     alignItems: 'center',
     marginBottom: 20,
+    width: '100%',
+    zIndex: 10, // Importante para que el dropdown se muestre por encima de otros elementos
   },
   title: {
     fontSize: 20,
@@ -244,6 +311,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 20,
     width: '80%',
+    zIndex: 1, // Asegura que el botón esté por debajo del dropdown
   },
   buttonText: {
     color: '#fff',
@@ -285,5 +353,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginVertical: 10,
+  },
+  dropdown: {
+    width: '100%',
+    backgroundColor: '#fff',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  dropdownContainer: {
+    width: '100%',
+    backgroundColor: '#fff',
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
